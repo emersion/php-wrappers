456ef35ff7174f2062f578c755eb01c238e3f9f3
Emersion <contact@emersion.fr>
2014-11-10 15:22:10 +0100

    Fixes a bug with FtpsStream.

--- src/Wrappers/FtpsStream.php
+++ src/Wrappers/FtpsStream.php
@@ -10,7 +10,21 @@ namespace Wrappers;
 class FtpsStream extends FtpStream {
 	protected static $protocol = 'ftps';
 
-	protected static function conn_new($host, $port) {
-		return ftp_ssl_connect($host, $port);
+	protected static function conn_new($urlData) {
+		$conn = ftp_ssl_connect($urlData['host'], $urlData['port']);
+		if ($conn === false) {
+			return false;
+		}
+
+		if (!empty($urlData['user'])) {
+			if (!ftp_login($conn, $urlData['user'], $urlData['pass'])) {
+				return false;
+			}
+		}
+
+		// Turn passive mode on
+		ftp_pasv($conn, true);
+
+		return $conn;
 	}
 }
